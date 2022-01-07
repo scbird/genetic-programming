a8ea2c5f57b5bf88404065d2b6920d703590b9e1
Stefan Bird <stefan.bird@emigrad.com>
2022-01-08 08:17:49 +1100

    Revert "chore: optimise distance calculations"
    This reverts commit b838310d654190ab2aceb85069f3503e054120b2.

--- src/program/selectors/creatures.ts
+++ src/program/selectors/creatures.ts
@@ -1,11 +1,12 @@
 import { AnyAction } from 'redux'
-import { createSelector } from 'reselect'
 import { getRequestedAction } from '../getRequestedAction'
 import { BoardState, Creature, Plant } from '../types'
 import {
   getDistance,
   getHeading,
+  getNearestTo,
   getRelativeAngle,
+  HasLocation,
   normalizeHeading
 } from '../util'
 import { getCreatures, getPlants } from './board'
@@ -15,66 +16,35 @@ const MAX_EAT_ANGLE = Math.PI / 4
 const PLANT_SCORE = 1
 const CREATURE_SCORE = 5
 
-const getDistanceFromCreature = createSelector(getCreatures, (creatures) => {
-  const distances: Record<string, number> = {}
-
-  return (creatureId: number, object: Creature | Plant): number => {
-    const key =
-      creatureId < object.id
-        ? `${object.type}:${creatureId}:${object.id}`
-        : `${object.type}:${object.id}:${creatureId}`
-
-    if (distances[key] === undefined) {
-      distances[key] = getDistance(creatures[creatureId], object)
-    }
-
-    return distances[key]
-  }
-})
-
 export function getClosestCreatureDistance(
   state: BoardState,
   id: number
 ): number {
-  const closestCreature = getClosestCreatureTo(state)(id)
+  const creature = getCreatures(state)[id]
 
-  return closestCreature
-    ? getDistanceFromCreature(state)(id, closestCreature)
-    : Infinity
+  return getDistance(creature, getClosestCreatureTo(state, id))
 }
 
 export function getClosestCreatureAngle(state: BoardState, id: number): number {
   const creature = getCreatures(state)[id]
-  const closestCreature = getClosestCreatureTo(state)(id)
+  const closest = getClosestCreatureTo(state, id)
+  const headingToClosest = getHeading(creature, closest)
 
-  if (closestCreature) {
-    const headingToClosest = getHeading(creature, closestCreature)
-
-    return getRelativeAngle(creature.heading, headingToClosest)
-  } else {
-    return 0
-  }
+  return getRelativeAngle(creature.heading, headingToClosest)
 }
 
 export function getClosestPlantDistance(state: BoardState, id: number): number {
-  const closestPlant = getClosestPlantTo(state)(id)
+  const creature = getCreatures(state)[id]
 
-  return closestPlant
-    ? getDistanceFromCreature(state)(id, closestPlant)
-    : Infinity
+  return getDistance(creature, getClosestPlantTo(state, id))
 }
 
 export function getClosestPlantAngle(state: BoardState, id: number): number {
   const creature = getCreatures(state)[id]
-  const closestPlant = getClosestPlantTo(state)(id)
+  const closestPlant = getClosestPlantTo(state, id)
+  const headingToClosest = getHeading(creature, closestPlant)
 
-  if (closestPlant) {
-    const headingToClosest = getHeading(creature, closestPlant)
-
-    return getRelativeAngle(creature.heading, headingToClosest)
-  } else {
-    return 0
-  }
+  return getRelativeAngle(creature.heading, headingToClosest)
 }
 
 export function getCreatureExpression(state: BoardState, id: number): string {
@@ -85,7 +55,7 @@ export function getWouldEat(
   state: BoardState,
   id: number
 ): Creature | Plant | null {
-  return getWouldEatCreature(state)(id) ?? getWouldEatPlant(state)(id)
+  return getWouldEatCreature(state, id) ?? getWouldEatPlant(state, id)
 }
 
 export function getDesiredActions(state: BoardState): AnyAction[] {
@@ -102,112 +72,52 @@ export function getCreatureScore(creature: Creature): number {
   )
 }
 
-const getClosestCreatureTo = createSelector(
-  getDistanceFromCreature,
-  getCreatures,
-  (distanceFromCreature, creatures) => {
-    const closestTo: Record<number, Creature> = {}
+function getWouldEatPlant(state: BoardState, id: number) {
+  return getWouldEatObject(
+    getCreatures(state)[id],
+    getPlants(state).filter((plant) => plant.diedAt === null)
+  )
+}
 
-    return (id: number): Creature | null => {
-      if (!closestTo[id]) {
-        const eligableCreatures = creatures.filter(
-          (creature) => isAlive(creature) && creature.id !== id
-        )
-        const distances = eligableCreatures.map((creature) =>
-          distanceFromCreature(id, creature)
-        )
-        const minDistance = Math.min(...distances)
-        const closestIdx = distances.findIndex(
-          (distance) => distance === minDistance
-        )
+function getWouldEatCreature(state: BoardState, id: number) {
+  const creatures = getCreatures(state)
 
-        closestTo[id] = eligableCreatures[closestIdx] ?? null
-      }
-
-      return closestTo[id]
-    }
-  }
-)
-
-const getClosestPlantTo = createSelector(
-  getDistanceFromCreature,
-  getCreatures,
-  getPlants,
-  (distanceFromCreature, creatures, plants) => {
-    const closestTo: Record<number, Plant> = {}
-    const eligablePlants = plants.filter(isAlive)
-
-    return (id: number): Plant | null => {
-      if (!closestTo[id]) {
-        const distances = eligablePlants.map((plant) =>
-          distanceFromCreature(id, plant)
-        )
-        const minDistance = Math.min(...distances)
-        const closestIdx = distances.findIndex(
-          (distance) => distance === minDistance
-        )
-
-        closestTo[id] = eligablePlants[closestIdx] ?? null
-      }
-
-      return closestTo[id]
-    }
-  }
-)
-
-const getWouldEatObject = createSelector(
-  getDistanceFromCreature,
-  (distanceFromCreature) => <T extends Creature | Plant>(
-    creature: Creature,
-    objects: readonly T[]
-  ) => {
-    const objectDistances = objects
-      .map(
-        (curr) => [curr, distanceFromCreature(creature.id, curr)] as [T, number]
-      )
-      .filter(([curr, distance]) => {
-        const headingToObject = getHeading(creature, curr)
-        const angleToObject = normalizeHeading(
-          creature.heading - headingToObject
-        )
-
-        return (
-          distance <= MAX_EAT_DISTANCE &&
-          Math.abs(angleToObject) <= MAX_EAT_ANGLE
-        )
-      })
-      .sort((a, b) => a[1] - b[1])
-
-    return objectDistances[0]?.[0] ?? null
-  }
-)
-
-const getWouldEatPlant = createSelector(
-  getCreatures,
-  getPlants,
-  getWouldEatObject,
-  (creatures, plants, wouldEatObject) => (id: number) => {
-    const creature = creatures[id]
-
-    return wouldEatObject(creature, plants.filter(isAlive))
-  }
-)
-
-const getWouldEatCreature = createSelector(
-  getCreatures,
-  getWouldEatObject,
-  (creatures, wouldEatObject) => (id: number) => {
-    const creature = creatures[id]
-
-    return wouldEatObject(
-      creature,
-      creatures.filter(
-        (otherCreature) => otherCreature !== creature && isAlive(otherCreature)
-      )
+  return getWouldEatObject(
+    creatures[id],
+    creatures.filter(
+      (creature) => creature.diedAt === null && creature.id !== id
     )
-  }
-)
+  )
+}
 
-function isAlive(object: Creature | Plant): boolean {
-  return object.diedAt === null
+function getWouldEatObject<T extends HasLocation>(
+  creature: Creature,
+  objects: readonly T[]
+): T | null {
+  const objectDistances = objects
+    .map((curr) => [curr, getDistance(creature, curr)] as [T, number])
+    .filter(([curr, distance]) => {
+      const headingToObject = getHeading(creature, curr)
+      const angleToObject = normalizeHeading(creature.heading - headingToObject)
+
+      return (
+        distance <= MAX_EAT_DISTANCE && Math.abs(angleToObject) <= MAX_EAT_ANGLE
+      )
+    })
+    .sort((a, b) => a[1] - b[1])
+
+  return objectDistances[0]?.[0] ?? null
+}
+
+function getClosestCreatureTo(state: BoardState, id: number): Creature {
+  const creatures = getCreatures(state)
+
+  return getNearestTo(
+    creatures[id],
+    creatures.filter((creature) => creature.id !== id)
+  )
+}
+
+function getClosestPlantTo(state: BoardState, id: number): Plant {
+  return getNearestTo(getCreatures(state)[id], getPlants(state))
 }
